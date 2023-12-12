5716c8dcd7134cf723cdac2cb4655d48ce1f08d9
Sree <Skarmoney99@gmail.com>
2023-12-11 19:13:34 -0600

    Update AI.py

--- AI.py
+++ AI.py
@@ -38,7 +38,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}<style>", unsafe_allow_html=True)
 
-local_css("style/style.css")
+local_css("style.css")
 
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
